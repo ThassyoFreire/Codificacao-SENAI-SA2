2a33084331bd2841929e1200a905868f106009f9
ThassyoFreire <121192273+ThassyoFreire@users.noreply.github.com>
2023-07-19 15:38:09 -0300

    Add files via upload

---
+++ Pessoa_Fisica.cs
@@ -0,0 +1,8 @@
+namespace ATividade
+{
+    class Pessoa_Fisica : Clientes
+    {
+        public string cpf;
+        public string rg;
+    }
+}
---
+++ Pessoa_Juridica.cs
@@ -0,0 +1,15 @@
+namespace Atividade
+{
+    class Pessoa_Juridica
+    {
+        public string cnpj;
+        public string ie;
+
+        public override void Pagar_Imposto(float v)
+        {
+            this.valor = v;
+            this.valor_imposto = this.valor * 20 / 100;
+            this.total = this.valor + this.valor_imposto;
+        }
+    }
+}
