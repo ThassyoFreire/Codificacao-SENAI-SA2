377fd53857ebb9325a0252ac9e84f2caa662823f
ThassyoFreire <121192273+ThassyoFreire@users.noreply.github.com>
2023-07-19 15:43:50 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace Atividade
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Pessoa_Fisica pf = new Pessoa_Fisica();
+            pf.cpf = "123.456.789-00";
+        }
+    }
+}
